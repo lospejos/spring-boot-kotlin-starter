c7bc16c615acdf82efc2d3e260f8065934f8c2c5
Rustam <ismailov.rustam@gmail.com>
2018-03-03 20:31:01 +0200

    SpringBoot 2.0.0 Final,Kotlin 1.2.30

--- src/main/kotlin/org/hidetake/app/App.kt
+++ src/main/kotlin/org/hidetake/app/App.kt
@@ -3,7 +3,7 @@ package org.hidetake.app
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
-@SpringBootApplication(scanBasePackages = arrayOf("org.hidetake.app"))
+@SpringBootApplication(scanBasePackages = ["org.hidetake.app"])
 open class App {
     companion object {
         @JvmStatic
